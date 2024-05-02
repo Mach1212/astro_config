2a3c74b4c228601cd4b2d2cbf408183ce66d22ed
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-02 05:54:31 +0000

    [Fix] Transparency toggle not working

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -110,8 +110,8 @@ return {
           function() require("astrocore").toggle_term_cmd "sudo lazydocker" end,
           desc = "ToggleTerm LazyDocker",
         },
-        
-        ["<leader>uT"] = { "<cmd>ToggleTransparency<cr>", desc = "Toggle transparency" },
+
+        ["<Leader>uT"] = { "<cmd>TransparentToggle<cr>", desc = "Toggle transparency" },
       },
       t = {
         -- setting a mapping to false will disable it
